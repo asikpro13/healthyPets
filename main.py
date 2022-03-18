e44cb34172be6f10fd6916aa95ffebbc63da08ec
vadimmazurek <asikpro13@mail.ru>
2022-03-19 02:52:23 +0500

    add icon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, url_for
+import os
 
 
 application = Flask(__name__)
@@ -7,6 +8,12 @@ application = Flask(__name__)
 @application.route('/')
 def main():
     return render_template('main.html')
+
+
+@application.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(application.root_path, 'static'),
+                          'favicon.png', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=5001, debug=True)
